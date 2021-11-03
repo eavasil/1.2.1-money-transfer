5e955cd2aa78f6c056b40150bad3b77fafbc8223
eavasil <dan_kat@mail.ru>
2021-11-03 13:21:23 +0700

    баг перевода

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int accountBalance = 2_000_000_000;
+        int transferPlus = 500_000_000;
+        int currentBalance = accountBalance + transferPlus;
+        System.out.println(currentBalance);
+    }
+}
